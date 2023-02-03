47446555104eb46ebfa2a211e45cc8ffb9019ce6
Ipuch <pierre.puchaud@umontreal.ca>
2023-02-03 15:29:22 -0500

    for kevin

--- bioptim/limits/penalty.py
+++ bioptim/limits/penalty.py
@@ -661,50 +661,6 @@ class PenaltyFunctionAbstract:
 
             return marker_objective
 
-        # Todo : Question : que signifi le _rt
-        #                 : appeler la fonction minimize force difference plutôt ?
-        #                 Est-ce que minimize est uniquement pour la minimisation
-        #                 d'une valeur et non pas pour une différence?
-        @staticmethod
-        def track_muscle_force_custom(
-                penalty: PenaltyOption, all_pn: PenaltyNodeList, force: Union[int, float], rt: int
-        ):
-            """
-            Minimize the difference of the model muscle force produiced and the targeted muscle force
-            By default this function is quadratic, meaning that it minimizes the difference.
-
-            Parameters
-            ----------
-            penalty: PenaltyOption
-                The actual penalty to declare
-            all_pn: PenaltyNodeList
-                The penalty node elements
-            force: Union[int, str]
-                The name or index of the segment
-            rt: int
-                The index of the RT in the bioMod
-            """
-            from ..interfaces.biorbd_model import BiorbdModel
-
-            penalty.quadratic = True if penalty.quadratic is None else penalty.quadratic
-
-            nlp = all_pn.nlp
-            force_index = nlp.model.segment_index(force) if isinstance(force, str) else force
-
-            if not isinstance(nlp.model, BiorbdModel):
-                raise NotImplementedError(
-                    "The track_muscle_force_custom penalty can only be called with a BiorbdModel"
-                )
-            model: BiorbdModel = nlp.model
-
-            current_force = nlp.model  # Comment recuperer la force ?
-            # DingModel.system_dynamics[1] ?
-
-            force_diff = (current_force - force)**2
-
-            force_objective = nlp.mx_to_cx(f"track_force", force_diff, nlp.states["q"])  # nlp.states["q"] ??
-            return force_objective
-
         @staticmethod
         def continuity(penalty: PenaltyOption, all_pn: Union[PenaltyNodeList, list]):
 
